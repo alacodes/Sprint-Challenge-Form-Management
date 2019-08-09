f068e81a58fcaa8ac3558960b92c5693c318cf25
Tracie T <traciecodes@gmail.com>
2019-08-09 11:50:00 -0500

    3rd: Basic registration form complete.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Formik, Field, Form as FormikForm, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -34,8 +34,50 @@ class App extends React.Component {
         onSubmit={ fields=> {
           alert('Nice to see you!\n' + JSON.stringify(fields, null, 4))
         }}
-        
+        render = {({ errors, status, touched }) => (
+          <FormikForm>
+            <div className = 'form-group'>
+              <label htmlFor='firstName'>First Name</label>
+              <Field name='firstName' type='text' className={'form-control' +(errors.firstName && touched.firstName ? ' is-invalid' : '')} />
+              <ErrorMessage name='firstName' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <label htmlFor='lastName'>Last Name</label>
+              <Field name='lastName' type='text' className={'form-control' +(errors.lastName && touched.lastName ? ' is-invalid' : '')} />
+              <ErrorMessage name='lastName' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <label htmlFor='username'>Username</label>
+              <Field name='username' type='text' className={'form-control' +(errors.username && touched.username ? ' is-invalid' : '')} />
+              <ErrorMessage name='username' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <label htmlFor='email'>Email</label>
+              <Field name='email' type='text' className={'form-control' +(errors.email && touched.email ? ' is-invalid' : '')} />
+              <ErrorMessage name='email' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <label htmlFor='password'>Password</label>
+              <Field name='password' type='password' className={'form-control' +(errors.password && touched.password ? ' is-invalid' : '')} />
+              <ErrorMessage name='password' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <label htmlFor='confirmPassword'>Confirm Password</label>
+              <Field name='confirmPassword' type='password' className={'form-control' +(errors.confirmPassword && touched.confirmPassword ? ' is-invalid' : '')} />
+              <ErrorMessage name='confirmPassword' component='div' className='invalid-feedback' />
+            </div>
+            <div className = 'form-group'>
+              <button type='submit' className='btn-primary'>Register/Sign-in</button>
+              <button type='reset' className='btn-reset'>Reset</button>
+            </div>
+
+          </FormikForm>
+        )}
+      />
     )
   }
 }
+
+  
+
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-render(
-    <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
